95ffe910225e70c75c6664653549122839570289
PKerty <pcurti61@gmail.com>
2024-08-14 13:35:05 -0300

    added nvterm and changed colorscheme

--- lua/pkerty/plugins/colorscheme.lua
+++ lua/pkerty/plugins/colorscheme.lua
@@ -1,30 +1,35 @@
 return {
-	"AlexvZyl/nordic.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("nordic").setup({
-			bold_keywords = true,
-			reduced_blue = true,
-			cursorline = {
-				-- Bold font in cursorline.
-				bold = true,
-				-- Bold cursorline number.
-				bold_number = true,
-				-- Avialable styles: 'dark', 'light'.
-				theme = "dark",
-				-- Blending the cursorline bg with the buffer bg.
-				blend = 0.85,
-				noice = {
-					-- Available styles: `classic`, `flat`.
-					style = "flat",
-				},
-				telescope = {
-					-- Available styles: `classic`, `flat`.
-					style = "flat",
-				},
-			},
-		})
-		require("nordic").load()
-	end,
+	-- "nyoom-engineering/oxocarbon.nvim",
+	-- config = function()
+	-- 	vim.opt.background = "dark" -- set this to dark or light
+	-- 	vim.cmd("colorscheme oxocarbon")
+	-- end,
+	-- "AlexvZyl/nordic.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("nordic").setup({
+	-- 		bold_keywords = true,
+	-- 		reduced_blue = true,
+	-- 		cursorline = {
+	-- 			-- Bold font in cursorline.
+	-- 			bold = true,
+	-- 			-- Bold cursorline number.
+	-- 			bold_number = true,
+	-- 			-- Avialable styles: 'dark', 'light'.
+	-- 			theme = "dark",
+	-- 			-- Blending the cursorline bg with the buffer bg.
+	-- 			blend = 0.85,
+	-- 			noice = {
+	-- 				-- Available styles: `classic`, `flat`.
+	-- 				style = "flat",
+	-- 			},
+	-- 			telescope = {
+	-- 				-- Available styles: `classic`, `flat`.
+	-- 				style = "flat",
+	-- 			},
+	-- 		},
+	-- 	})
+	-- 	require("nordic").load()
+	-- end,
 }
--- lua/pkerty/plugins/telescope.lua
+++ lua/pkerty/plugins/telescope.lua
@@ -10,26 +10,24 @@ return {
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 		local builtin = require("telescope.builtin")
-		telescope.setup {
-			defaults = { file_ignore_patterns = {"node_modules"}},
+		telescope.setup({
+			defaults = { file_ignore_patterns = { "node_modules" } },
 			pickers = {
 				find_files = {
-					hidden = true
-				}
-			}
-		}
-
+					hidden = true,
+				},
+			},
+		})
 
 		telescope.load_extension("fzf")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>ps', function()
+		keymap.set("n", "<leader>pf", builtin.find_files, {})
+		keymap.set("n", "<C-p>", builtin.git_files, {})
+		keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
+		keymap.set("n", "<leader>fb", builtin.buffers, {})
 	end,
 }
